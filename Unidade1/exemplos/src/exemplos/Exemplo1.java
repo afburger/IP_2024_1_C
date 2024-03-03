c58f3c8029fbdc589d6ffa994b714b860656467c
André <andre@iburger.com.br>
2024-03-03 18:56:16 -0300

    ✨ feat: Exemplos Unidade 01

---
+++ Unidade1/exemplos/src/exemplos/Exemplo1.java
@@ -0,0 +1,25 @@
+package exemplos;
+public class Exemplo1 {
+
+    public static void main(String[] args) {
+        // Entradas (Dandos iniciais)
+        double nota1 = 7;
+        double nota2 = 8;
+        double nota3 = 9;
+
+        // proesso (Dado intermediario)
+        double somaNotas = nota1 + nota2 + nota3;
+
+        // Saída
+        double media = somaNotas / 3;
+
+        System.out.println(media);
+
+    }
+
+
+   double nota1 = 7;
+   double nota2 = 5;
+   double nota3 = 9;
+
+}
---
+++ Unidade1/exemplos/src/exemplos/Exemplo2.java
@@ -0,0 +1,17 @@
+package exemplos;
+public class Exemplo2 {
+    
+    public static void main(String[] args) {
+        
+        // entradas
+        double comprimento = 7;
+        double largura = 9;
+
+        // saida
+        double area = comprimento * largura;
+
+        System.out.println(area);
+
+    }
+
+}
