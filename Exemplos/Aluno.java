31cd7b987c264355d2965fcc353d393a23b2a90f
André <andre@iburger.com.br>
2024-03-06 19:52:42 -0300

    Exemplos

---
+++ Exemplos/Aluno.java
@@ -0,0 +1,17 @@
+package exemplo;
+
+public class Aluno {
+
+	float nota1;
+	float nota2;
+	float nota3;
+	String nome;
+	char sexo;
+	
+	float calculaMedia() {
+		float soma = nota1 + nota2 + nota3;
+		float media = soma / 3;
+		return media;
+	}
+	
+}
